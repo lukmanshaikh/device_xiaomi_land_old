2c8567f8f4a4c6b4b63fec5bcafb466d5ffe79e6
Lukman Shaikh <shaikhlukman01@gmail.com>
2018-03-12 09:47:13 +0000

    Revert "land: Build the binderized power HAL"
    This reverts commit 992dde65089d2ee6e89f09b3a7ef59e9504c8923.

--- device.mk
+++ device.mk
@@ -337,8 +337,15 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/perf-profile0.conf:$(TARGET_COPY_OUT_VENDOR)/etc/perf/perf-profile0.conf
 
 # Power
+
+# Power HAL
 PRODUCT_PACKAGES += \
-    android.hardware.power@1.0-service.land
+    power.msm8937
+
+# Power HIDL
+PRODUCT_PACKAGES += \
+    android.hardware.power@1.0-impl \
+    android.hardware.power@1.0-service
 
 # RCS
 PRODUCT_PACKAGES += \

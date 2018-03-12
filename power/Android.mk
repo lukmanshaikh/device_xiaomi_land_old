589c88661ade45ccbaaa1ee3bc29fd09d653ddd6
Lukman Shaikh <shaikhlukman01@gmail.com>
2018-03-12 09:52:25 +0000

    Revert "land: power: Adapt for land"
    This reverts commit bd58b26934ed23330c60bbbe6782c1993218c18d.

--- power/Android.mk
+++ power/Android.mk
@@ -13,7 +13,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-ifeq ($(TARGET_DEVICE),land)
+ifeq ($(call my-dir),$(call project-path-for,qcom-power))
 
 LOCAL_PATH := $(call my-dir)
 
@@ -46,15 +46,69 @@ LOCAL_C_INCLUDES := external/libxml2/include \
                     external/icu/icu4c/source/common
 
 # Include target-specific files.
+ifeq ($(call is-board-platform-in-list, msm8974), true)
+LOCAL_SRC_FILES += power-8974.c
+endif
+
+ifeq ($(call is-board-platform-in-list, msm8226), true)
+LOCAL_SRC_FILES += power-8226.c
+endif
+
+ifeq ($(call is-board-platform-in-list, msm8610), true)
+LOCAL_SRC_FILES += power-8610.c
+endif
+
+ifeq ($(call is-board-platform-in-list, apq8084), true)
+LOCAL_SRC_FILES += power-8084.c
+endif
+
+ifeq ($(call is-board-platform-in-list, msm8994), true)
+LOCAL_SRC_FILES += power-8994.c
+endif
+
+ifeq ($(call is-board-platform-in-list, msm8996), true)
+LOCAL_SRC_FILES += power-8996.c
+endif
+
 ifeq ($(call is-board-platform-in-list,msm8937), true)
-LOCAL_SRC_FILES += power-8937.c
+LOCAL_SRC_FILES += power-8952.c
+endif
+
+ifeq ($(call is-board-platform-in-list,msm8952), true)
+LOCAL_SRC_FILES += power-8952.c
+endif
+
+ifeq ($(call is-board-platform-in-list,msm8953), true)
+LOCAL_SRC_FILES += power-8953.c
+endif
+
+ifeq ($(call is-board-platform-in-list,msm8998 apq8098_latv), true)
+LOCAL_SRC_FILES += power-8998.c
+endif
+
+ifeq ($(call is-board-platform-in-list,sdm660), true)
+LOCAL_SRC_FILES += power-660.c
+endif
+
+ifeq ($(call is-board-platform-in-list,sdm845), true)
+LOCAL_SRC_FILES += power-845.c
+endif
+
+ifeq ($(call is-board-platform-in-list, msm8909), true)
+LOCAL_SRC_FILES += power-8909.c
+endif
+
+ifeq ($(call is-board-platform-in-list,msm8916), true)
+LOCAL_SRC_FILES += power-8916.c
 endif
 
 ifneq ($(TARGET_POWER_SET_FEATURE_LIB),)
     LOCAL_STATIC_LIBRARIES += $(TARGET_POWER_SET_FEATURE_LIB)
 endif
 
-LOCAL_CFLAGS += -DINTERACTION_BOOST
+ifeq ($(TARGET_USES_INTERACTION_BOOST),true)
+    LOCAL_CFLAGS += -DINTERACTION_BOOST
+endif
 
 ifneq ($(TARGET_POWERHAL_SET_INTERACTIVE_EXT),)
 LOCAL_CFLAGS += -DSET_INTERACTIVE_EXT
@@ -63,6 +117,10 @@ endif
 
 ifneq ($(TARGET_TAP_TO_WAKE_NODE),)
     LOCAL_CFLAGS += -DTAP_TO_WAKE_NODE=\"$(TARGET_TAP_TO_WAKE_NODE)\"
+endif
+
+ifeq ($(TARGET_HAS_LEGACY_POWER_STATS),true)
+    LOCAL_CFLAGS += -DLEGACY_STATS
 endif
 
 ifneq ($(TARGET_RPM_STAT),)
@@ -77,10 +135,20 @@ ifneq ($(TARGET_RPM_SYSTEM_STAT),)
     LOCAL_CFLAGS += -DRPM_SYSTEM_STAT=\"$(TARGET_RPM_SYSTEM_STAT)\"
 endif
 
-LOCAL_MODULE := android.hardware.power@1.0-service.land
-LOCAL_INIT_RC := android.hardware.power@1.0-service.land.rc
-LOCAL_SHARED_LIBRARIES += android.hardware.power@1.0
+ifneq ($(TARGET_WLAN_POWER_STAT),)
+    LOCAL_CFLAGS += -DWLAN_POWER_STAT=\"$(TARGET_WLAN_POWER_STAT)\"
+endif
 
+ifeq ($(TARGET_HAS_NO_WIFI_STATS),true)
+LOCAL_MODULE := android.hardware.power@1.0-service-qti
+LOCAL_INIT_RC := android.hardware.power@1.0-service-qti.rc
+LOCAL_SHARED_LIBRARIES += android.hardware.power@1.0
+LOCAL_CFLAGS += -DV1_0_HAL
+else
+LOCAL_MODULE := android.hardware.power@1.1-service-qti
+LOCAL_INIT_RC := android.hardware.power@1.1-service-qti.rc
+LOCAL_SHARED_LIBRARIES += android.hardware.power@1.1
+endif
 LOCAL_SHARED_LIBRARIES += vendor.lineage.power@1.0
 LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE_OWNER := qcom

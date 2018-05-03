c3fbe6f50ae902429ed1a882c15a0a80e7de62ee
Lukman Shaikh <shaikhlukman01@gmail.com>
2018-05-03 10:18:23 +0000

    omni changes

--- device.mk
+++ device.mk
@@ -124,6 +124,9 @@ PRODUCT_PACKAGES += \
     libshim_camera
 
 PRODUCT_PACKAGES += \
+    snap
+
+PRODUCT_PACKAGES += \
     camera.device@1.0-impl \
     camera.device@3.2-impl \
     android.hardware.camera.provider@2.4-impl \
